0965c5fdfc6e4db9ea2cfbd4dcc81e4f13597e3f
Michael Carr <mcarr2468@gmail.com>
2023-11-21 16:17:25 -0600

    add howItWorks route and page

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Error from './pages/Error';
 import Search from './pages/Search';
 import SingleItem from './pages/SingleItem';
 import About from './pages/About';
+import HowItWorks from './pages/HowItWorks';
 
 // components
 import Navbar from './components/Navbar';
@@ -30,6 +31,7 @@ const router = createBrowserRouter([
       { index: true, element: <Home /> },
       { path: 'search', element: <Search /> },
       { path: 'about', element: <About /> },
+      { path: 'howItWorks', element: <HowItWorks /> },
       { path: 'singleItem/:itemId', element: <SingleItem /> },
       // { path: '*', element: <Navigate to="/" /> }, // Redirect to Home for unknown routes
     ],
---
+++ src/pages/HowItWorks.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const HowItWorks = () => {
+    // want to have video w/ voice over showing how ot use the site
+  return (
+    <div>HowItWorks</div>
+  )
+}
+
+export default HowItWorks
