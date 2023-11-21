579edb3c66d2daaf93ca4da01ee051942b4ac065
Michael Carr <mcarr2468@gmail.com>
2023-11-21 09:57:42 -0600

    create pages folder and all the components for the pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <div className=' text-blue-500'>Home</div>
+    <div className=' bg-blue-500 w-20 h-20'></div>
+
+    </>
+  )
+}
+
+export default Home
