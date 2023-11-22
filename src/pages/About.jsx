e7c71de30f31e40e7c7435da945ed841778f62cb
Michael Carr <mcarr2468@gmail.com>
2023-11-22 10:45:50 -0600

    fixed height

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 const About = () => {
 
   return (
-    <div className="min-h-[100vh] text-center flex flex-row justify-center items-start lg:items-center lg:mt-0 mt-10">
+    <div className="min-h-[90vh] text-center flex flex-row justify-center items-start lg:items-center lg:mt-0 mt-10">
     <p className='max-w-[90%] lg:max-w-[60vh] text-2xl mr-0 lg:mr-6'>
     Welcome to GivingPlusOne, a website dedicated to helping donors find and support high-impact charities.
 Since a few days ago, we have been providing in-depth information and evaluations of charities, empowering donors to make informed decisions about where to give their money. We believe that transparency and accountability are essential to building trust in the charitable sector and ensuring that donor support is used effectively.
