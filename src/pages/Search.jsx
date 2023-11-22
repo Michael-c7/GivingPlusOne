9c47a60f2e51d09703ba8d1c04189d93bd83efe5
Michael Carr <mcarr2468@gmail.com>
2023-11-22 14:34:14 -0600

    add fallback default data and basic loading state

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -7,6 +7,9 @@ import { useStandardContext } from '../standard_context';
 import { Link } from 'react-router-dom';
 
 
+import defaultCoverImg from "../assets/images/defaultBackground.jpg"
+
+
 
 const Search = () => {
   const { searchInputValue, setSearchInputValue, setUserId} = useStandardContext()
@@ -15,6 +18,7 @@ const Search = () => {
   const [amountToGet, setAmountToGet] = React.useState(25)
 
   const [charityData, setCharityData] = React.useState([])
+  const [isCharityDataLoading, setIsCharityDataLoading] = React.useState(true)
 
   // console.log(searchInputValue)
   // get value from location search & causes checkboxes
@@ -25,6 +29,7 @@ const Search = () => {
 
     try {
       const response = await fetch(url);
+      setIsCharityDataLoading(true)
       
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
@@ -33,8 +38,11 @@ const Search = () => {
       const data = await response.json();
       const { nonprofits } = data
       // console.log(nonprofits);
+      setIsCharityDataLoading(false)
       setCharityData(nonprofits)
+
     } catch (error) {
+      setIsCharityDataLoading(false)
       console.error('Error fetching data:', error.message);
     }
   }
@@ -84,22 +92,31 @@ const Search = () => {
             </div>
           </header>
           {/* main, where cards go */}
-          <div className='grid gap-8  '>
-            {charityData.map((item, index) => {
+          <div className='grid gap-8'>
+            {/* Loading */}
+            {isCharityDataLoading & charityData.length < 1 ? (
+              <h1 className='font-semibold'>Loading...</h1>
+            ) : ""}
             
+            {/* Actual data */}
+            {charityData.map((item, index) => {
+              let defaultDescription = "We don't know a lot about them but we're sure their great. Click on the Details button to learn more information."
+
               return (
                 <div key={item.ein} className='bg-gray-100 shadow-[rgba(25,_25,_25,_0.10)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px]'>
                   {/* img */}
-                  <Link>
-                    <img src={item.coverImageUrl} alt={item.name} className='min-w-full max-h-[20rem] object-cover'/>
+                  <Link to={`/singleItem/${item.ein || index}`} onClick={() => setUserId(item.ein)}>
+                    <img src={item.coverImageUrl || defaultCoverImg} alt={item.name || "cover image"} className='min-w-full max-h-[20rem] object-cover'/>
                   </Link>
                   {/* info */}
                   <div className='p-4'>
                     <span>
-                      <h2 className='font-semibold text-xl mb-1'>{item.name}</h2>
-                      <h3 className='font-semibold'>{item.location}</h3>
+                      <h2 className='font-semibold text-xl mb-1'>{item.name || "Location unavailable"}</h2>
+                      <h3 className='font-semibold'>{item.location || "Location unavailable"}</h3>
                     </span>
-                    <div className='mt-6 mb-12'>{item.description}</div>
+                    <div className='mt-6 mb-12'>
+                    {item.description || defaultDescription}
+                    </div>
 
                     {/* btns */}
                     <div className='flex justify-between items-center'>
@@ -113,9 +130,11 @@ const Search = () => {
           </div>
 
           {/* Load more charities button */}
-          <div className='flex justify-center items-center p-4'>
-            <button className='btn-secondary' onClick={() => setCurrentPage(currentPage + 1)}>Load more charities...</button>
-          </div>
+          {!isCharityDataLoading & !charityData.length < 1 ? (
+            <div className='flex justify-center items-center p-4'>
+              <button className='btn-secondary' onClick={() => setCurrentPage(currentPage + 1)}>Load more charities...</button>
+            </div>
+          ) : ""}
         </section>
       </div>
     </div>
