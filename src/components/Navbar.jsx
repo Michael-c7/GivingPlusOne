f21dff5dbd23b643c9841a40d1d590d585edb20c
Michael Carr <mcarr2468@gmail.com>
2023-11-24 10:50:18 -0600

    do mobile style for the links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   return (
     <nav className='shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px]'>
       {/* container */}
-      <div className='flex  flex-col sm:flex-row  justify-between items-center p-6 mx-12'>
+      <div className='flex flex-col sm:flex-row justify-between items-center p-6 mx-12'>
         <Link to="/"  className=''>
           <img src={givingPlusOneLogo} className="w-[90vw] max-w-[185px]" alt="givingPlusOne logo"/>
         </Link>
@@ -24,10 +24,10 @@ const Navbar = () => {
         </div> */}
 
         {/* links */}
-        <div className=' sm:mt-0 mt-2'>
-          <Link to="/">Home</Link>
-          <Link to="about" className='mx-6'>About</Link>
-          <Link to="howItWorks">How it Works</Link>
+        <div className='sm:mt-0 mt-2 flex flex-col sm:flex-row justify-between items-center'>
+          <Link to="/" className='sm:my-0 my-2'>Home</Link>
+          <Link to="about" className='mx-6 sm:my-0 my-2'>About</Link>
+          <Link to="howItWorks" className='sm:my-0 my-2'>How it Works</Link>
         </div>
       </div>
     </nav>
